b8ebafce94e8b6316a3581be574325de57d199b9
Prem Kumar R <premramaiah04@gmail.com>
2025-10-08 01:41:08 +0530

    first commit after setup env and deployment in production

---
+++ src/env/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBKQZn1VHpID3D8jpjQdSd3usBg7pxeeRA",
+  authDomain: "ripples-in-motion-activi-8f311.firebaseapp.com",
+  projectId: "ripples-in-motion-activi-8f311",
+  storageBucket: "ripples-in-motion-activi-8f311.firebasestorage.app",
+  messagingSenderId: "149110996777",
+  appId: "1:149110996777:web:ddf26759c826ddbb30c138",
+  measurementId: "G-4WHC505DY3"
+  }
+};
